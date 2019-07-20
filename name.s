172f6b53e85edc308101f3e172364f873b68af86
Codegitz <codegit@foxmail.com>
2019-07-20 14:00:06 +0800

    delete all

--- name.s
+++ name.s
@@ -1,2 +1,10 @@
-	Xshell_GEmdcwQlQQ.png
-	Xshell_QfYq2NLHde.png
+	deleted:    ShareX_BpdbQHQoTB.png
+	deleted:    Xshell_GEmdcwQlQQ.png
+	deleted:    Xshell_QfYq2NLHde.png
+	deleted:    cJc3usR7ai.png
+	deleted:    chrome_e1yQ8KelnC.png
+	deleted:    chrome_fxBFKPitSg.png
+	deleted:    chrome_xyjDjO7Mjc.png
+	deleted:    oVXVFZSGRk.gif
+	deleted:    obafX2yr8x.gif
+	deleted:    tLPShBe2N3.png
