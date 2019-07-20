95162520f30ca38080cc8583168c798fdef53f90
Codegitz <codegit@foxmail.com>
2019-07-20 12:28:22 +0800

    commit

--- upload.sh
+++ upload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git add .
-echo "请输入你的commit"
-read=commit
-git commit -m"commit"
+echo -e "请输入你的commit:"
+read commit
 
-git push origin master
+echo ${commit}
+git commit -m"commit"
+#git push origin master
 
 URL=https://github.com/codegitz/upload
 echo "图片URL为:${URL}"
