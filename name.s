6e0e671a94c625c06e909fb0c87386b95c58c80c
Codegitz <codegit@foxmail.com>
2019-07-20 13:43:20 +0800

    asfd

--- name.s
+++ name.s
@@ -1,1 +1,6 @@
-	chrome_LQMTyB6P7O.png
+	deleted:    1563354527050.png
+	deleted:    2r2x8rN5IO.gif
+	deleted:    7qNgdhF69G.gif
+	deleted:    chrome_LQMTyB6P7O.png
+	deleted:    chrome_e1yQ8KelnC.png
+	deleted:    chrome_fxBFKPitSg.png
